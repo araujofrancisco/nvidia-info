496608b658c821725cc3608b35c70ae8f98b3c26
Francisco Araujo <3943185+araujofrancisco@users.noreply.github.com>
2022-07-18 13:17:18 -0400

    Fix issues with clients names

--- getinfo.sh
+++ getinfo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mapfile -t client < clients.txt
+mapfile -t clients < clients.txt
 
 for client in "${clients[@]}"
 do
     result=$(ssh $client 'nvidia-smi | grep -E "[0-9]C"' | grep -v 'Phone' 2>&1)
-    echo -e "$miner -->\n$result"
+    echo -e "$client -->\n$result"
 done
