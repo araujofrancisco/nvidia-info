2044df5811faae40a98757b475cb8a7736121a82
Francisco Araujo <3943185+araujofrancisco@users.noreply.github.com>
2022-07-20 10:19:06 -0400

    Remove line from header that does not provide information

--- getinfo.sh
+++ getinfo.sh
@@ -4,8 +4,7 @@ mapfile -t clients < clients.txt
 showtitles="-B 5"
 for client in "${clients[@]}"
 do
-    cmd="nvidia-smi | grep $showtitles -E '[0-9]C' | grep -v 'Phone'"
-    #echo $cmd
+    cmd="nvidia-smi | grep $showtitles -E '[0-9]C' | grep -v 'NVIDIA GeForce' | grep -v 'Phone'"
     result=$(ssh $client $cmd 2>&1)
     showtitles=""
     echo -e "$client -->\n$result"
